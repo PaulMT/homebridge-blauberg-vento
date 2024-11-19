ab517011411eb30ce78dde62fc582002453cfce9
Paul_MT <PavloM.T@gmail.com>
2024-11-19 18:32:21 +0200

    fix filter status warning

--- src/accessory.ts
+++ src/accessory.ts
@@ -7,6 +7,7 @@ import { Device, DeviceStatus } from './device.js';
 
 export class VentoExpertAccessory {
   private service: Service;
+  private filterService: Service;
   private client: VentoExpertClient;
   private deviceStatus!: DeviceStatus;
 
@@ -21,6 +22,8 @@ export class VentoExpertAccessory {
       .setCharacteristic(this.platform.Characteristic.SerialNumber, device.deviceId);
 
     this.service = this.accessory.getService(this.platform.Service.Fanv2) || this.accessory.addService(this.platform.Service.Fanv2);
+    this.filterService = this.accessory.getService(this.platform.Service.FilterMaintenance)
+      || this.accessory.addService(this.platform.Service.FilterMaintenance);
 
     this.service.setCharacteristic(this.platform.Characteristic.Name, device.name);
 
@@ -41,7 +44,7 @@ export class VentoExpertAccessory {
     this.service.getCharacteristic(this.platform.Characteristic.SwingMode)
       .onSet(this.setSwingMode.bind(this));
 
-    this.service.getCharacteristic(this.platform.Characteristic.FilterLifeLevel)
+    this.filterService.getCharacteristic(this.platform.Characteristic.FilterLifeLevel)
       .setProps({
         minValue: 0,
         maxValue: 181,
@@ -65,11 +68,11 @@ export class VentoExpertAccessory {
   }
 
   async getActive(): Promise<CharacteristicValue> {
-    this.platform.log.debug('[%s] Get status', this.device.deviceId);
+    this.platform.log.debug('[%s] Get status', this.device.name);
     return this.client.getStatus()
       .then(status => {
         this.deviceStatus = status;
-        this.platform.log.debug('[%s] Status:', this.device.deviceId, status);
+        this.platform.log.debug('[%s] Status:', this.device.name, status);
         if (status.alarm === AlarmWarningIndicator.NO) {
           this.service.updateCharacteristic(this.platform.Characteristic.RotationSpeed, status.speed);
         } else {
@@ -77,38 +80,38 @@ export class VentoExpertAccessory {
           this.service.updateCharacteristic(this.platform.Characteristic.RotationSpeed, new Error('Device Alarm: ' + status.alarm));
         }
         this.service.updateCharacteristic(this.platform.Characteristic.SwingMode, status.mode);
-        this.service.updateCharacteristic(this.platform.Characteristic.FilterLifeLevel, status.filter.life);
-        this.service.updateCharacteristic(this.platform.Characteristic.FilterChangeIndication, status.filter.replace);
+        this.filterService.updateCharacteristic(this.platform.Characteristic.FilterLifeLevel, status.filter.life);
+        this.filterService.updateCharacteristic(this.platform.Characteristic.FilterChangeIndication, status.filter.replace);
         return status.active;
       })
       .catch(this.handleError.bind(this));
   }
 
   async setActive(value: CharacteristicValue) {
-    this.platform.log.debug('[%s] Turn on/off ->', this.device.deviceId, value);
+    this.platform.log.debug('[%s] Turn on/off ->', this.device.name, value);
     return this.client.turnOnOff(<UnitOnOff>value)
-      .then(active => this.platform.log.debug('[%s] Turned on/off:', this.device.deviceId, active))
+      .then(active => this.platform.log.debug('[%s] Turned on/off:', this.device.name, active))
       .catch(this.handleError.bind(this));
   }
 
   async setRotationSpeed(value: CharacteristicValue) {
     if (value !== 0) {
-      this.platform.log.debug('[%s] Change speed ->', this.device.deviceId, value);
+      this.platform.log.debug('[%s] Change speed ->', this.device.name, value);
       return this.client.changeSpeed(<SpeedNumber>value)
-        .then(speed => this.platform.log.debug('[%s] Speed changed:', this.device.deviceId, speed))
+        .then(speed => this.platform.log.debug('[%s] Speed changed:', this.device.name, speed))
         .catch(this.handleError.bind(this));
     }
   }
 
   async setSwingMode(value: CharacteristicValue) {
-    this.platform.log.debug('[%s] Change mode ->', this.device.deviceId, value);
+    this.platform.log.debug('[%s] Change mode ->', this.device.name, value);
     return this.client.changeMode(<VentilationMode>value)
-      .then(value => this.platform.log.debug('[%s] Mode changed:', this.device.deviceId, value))
+      .then(value => this.platform.log.debug('[%s] Mode changed:', this.device.name, value))
       .catch(this.handleError.bind(this));
   }
 
   private handleError(error: Error): Promise<CharacteristicValue> {
-    this.platform.log.warn('[%s] Client error:', this.device.deviceId, error.message);
+    this.platform.log.warn('[%s] Client error:', this.device.name, error.message);
     throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.OPERATION_TIMED_OUT);
   }
 }
