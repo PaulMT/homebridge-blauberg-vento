111217304da180d9bc5257476479dd03cf24672b
Paul_MT <PavloM.T@gmail.com>
2024-12-03 20:19:29 +0200

    minor changes to device discovery

--- src/platform.ts
+++ src/platform.ts
@@ -1,40 +1,54 @@
-import { API, Characteristic, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service } from 'homebridge';
+import type { API, Characteristic, DynamicPlatformPlugin, Logging, PlatformAccessory, PlatformConfig, Service } from 'homebridge';
 
-import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
 import { VentoExpertAccessory } from './accessory.js';
 import { Device } from './device.js';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
 
 export class BlaubergVentoPlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service;
   public readonly Characteristic: typeof Characteristic;
 
-  public readonly accessories: PlatformAccessory[] = [];
+  public readonly accessories: Map<string, PlatformAccessory> = new Map();
+  public readonly discoveredCacheUUIDs: string[] = [];
 
   constructor(
-    public readonly log: Logger,
+    public readonly log: Logging,
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
     this.Service = api.hap.Service;
     this.Characteristic = api.hap.Characteristic;
 
-    this.api.on('didFinishLaunching', () => this.discoverDevices());
+    this.log.debug('Finished initializing platform:', this.config.name);
+
+    this.api.on('didFinishLaunching', () => {
+      log.debug('Executed didFinishLaunching callback');
+
+      this.discoverDevices();
+    });
   }
 
   configureAccessory(accessory: PlatformAccessory) {
     this.log.info('Loading accessory from cache:', accessory.displayName);
-    this.accessories.push(accessory);
+    this.accessories.set(accessory.UUID, accessory);
   }
 
   discoverDevices() {
     if (this.config.devices) {
       this.config.devices.forEach(this.configureDevice.bind(this));
     }
+
+    for (const [uuid, accessory] of this.accessories) {
+      if (!this.discoveredCacheUUIDs.includes(uuid)) {
+        this.log.info('Removing existing accessory from cache:', accessory.displayName);
+        this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+      }
+    }
   }
 
   configureDevice(device: Device) {
     const uuid = this.api.hap.uuid.generate(device.deviceId);
-    const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+    const existingAccessory = this.accessories.get(uuid);
 
     if (existingAccessory) {
       this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
@@ -45,5 +59,7 @@ export class BlaubergVentoPlatform implements DynamicPlatformPlugin {
       new VentoExpertAccessory(this, accessory, device);
       this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
     }
+
+    this.discoveredCacheUUIDs.push(uuid);
   }
 }
